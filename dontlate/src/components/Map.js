9db818e398ae28b4695a08bdc9088616d9244aba
Lee-s-an <dltks2772@naver.com>
2023-06-24 02:07:51 +0900

    kakao

--- dontlate/src/components/Map.js
+++ dontlate/src/components/Map.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+const { kakao } = window;
 
 const Map = ({ stations }) => {
-  return <div></div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    var container = document.getElementById("map");
+    var options = {
+      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+      level: 5,
+    };
+    var map = new kakao.maps.Map(container, options);
+  }, []);
+
+  return (
+    <div className="w-full h-full relative">
+      <div id="map" style={{ width: "100%", height: "100%" }}></div>
+    </div>
+  );
 };
 
 export default Map;
